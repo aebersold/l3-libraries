fdb98766ad09acee3f4e654f4262859a64c32476
aebersold <simon@ximedia.ch>
2013-04-22 10:24:49 +0200

    added intial classes

---
+++ URL.php
@@ -0,0 +1,24 @@
+<?php 
+
+class URL extends \Laravel\URL {
+
+/**
+ * Generate an URL to an amazon S3 file.
+ * Requires a valid URL in the application config file
+ * use s3bucket_http, s3bucket_https 
+ *
+ * @param  string  $url
+ * @param  bool    $https
+ * @return string
+ */
+
+public static function to_s3($url, $https = false)
+{
+	if (static::valid($url) or static::valid('http:'.$url)) return $url;
+
+	$bucket = ($https == true ? Config::get('application.s3bucket_https') : Config::get('application.s3bucket_http'));
+
+	return $bucket.'/'.$url;
+}
+
+}
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+
+class Config extends \Laravel\Config {
+
+/**
+ * base code by matteo patisso, enhanced by simon aebersold
+ *
+ * write/edit a laravel config file
+ * the config files needs write access!
+ *
+ * @param  string  $file
+ * @param  string  $key
+ * @param  mixed   $val
+ * @return bool
+ */
+
+public static function write($file, $key, $val)
+{
+	Config::set($file . '.' . $key, $val);
+	$config = var_export(self::get(), true);
+	
+	if (file_put_contents('application/config/'.$file.'.php', "<?php return $config;") > 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
