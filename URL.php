cb9208ac200a129d936eba97b2fbe1e21fa99086
Simon Aebersold <simon@ximedia.ch>
2014-05-16 12:56:48 +0200

    refactored some old ugly code

--- URL.php
+++ URL.php
@@ -11,12 +11,17 @@ class URL extends \Laravel\URL {
 	 * @param  bool    $https
 	 * @return string
 	 */
-
 	public static function to_s3($url, $https = false)
 	{
-		if (static::valid($url) or static::valid('http:'.$url)) return $url;
+		if (static::valid($url) || static::valid('http:'.$url)) {
+			return $url;
+		}
 
-		$bucket = ($https == true ? Config::get('application.s3bucket_https') : Config::get('application.s3bucket_http'));
+		if($https == true) {
+			$bucket = Config::get('application.s3bucket_https');
+		} else {
+			$bucket = Config::get('application.s3bucket_http');
+		}
 
 		return $bucket.'/'.$url;
 	}
